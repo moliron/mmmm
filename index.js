6bfae89d3f331ba12506b54dfdac1e1b5afd51ea
vvv <ilyaromensky@gmail.com>
2024-09-13 15:58:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+//alert('Я JavaScript!')
+
+// let name 
+// let admin
+// naem = "Иван"
+// admin = naem
+// alert(admin)
+
+// let name = "Ilya";
+// alert( `hello ${1}` ); // ?
+// alert( `hello ${"name"}` ); // ?
+// alert( `hello ${name}` ); // ?
+
+// var Name = prompt("Как Вас зовут?");
+//     document.write("Привет, " + Name + "!");
+
+// let a = 1, b = 1;
+// let c = ++a; // ?
+// let d = b++; // ?
+// alert(b)
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// alert(x)
+
+// let a = +prompt("Первое число?", 1);
+// let b = +prompt("Второе число?", 2);
+// alert(a + b); // 12
+
+// 7 > 5
+// "стол" > "стул"
+// "4" > "52"
+// undefined == null
+// undefined === null
+// null == "\n0\n"
+// null === +"\n0\n"
+
+
+// if ("0") {
+//     alert( 'Привет' );
+//   }
+  
+
+// let value = prompt('Какое "официальное" название JavaScript?', '');
+//     if (value == 'ECMAScript') 
+//     {
+//         alert('Верно!');
+//     }
+//     else
+//     {
+//         alert('Не знаете? ECMAScript!');
+//     }
+    
+    
+// let value = prompt('Введите число')
+// if (value > 0) 
+//   {
+//     alert( 1 );
+//   } 
+//   else if (value < 0) 
+//   {
+//     alert( -1 );
+//   } 
+//   else 
+//   {
+//     alert( 0 );
+//   }
+
+let result;
+let a = +prompt('введите число')
+let b = +prompt('введите число')
+if (a + b < 4)
+{
+  result = 'Мало';
+} else {
+  result = 'Много';
+}
+alert(result)
